0ede428b7b6a53f6c42421946a9898947631b059
ZalTV Việt Nam <46188219+zaltvvn@users.noreply.github.com>
2026-03-14 14:55:55 +0700

    Update stream.js

--- api/stream.js
+++ api/stream.js
@@ -2,15 +2,14 @@ export default async function handler(req, res) {
 
   const ch = req.query.ch || "25";
 
-  const iframeUrl = `https://www.adintrend.tv/hd/live/i.php?ch=${ch}`;
+  const url = `https://www.adintrend.tv/hd/live/i.php?ch=${ch}`;
 
   try {
 
-    const r = await fetch(iframeUrl, {
+    const r = await fetch(url, {
       headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-        "Referer": "https://www.adintrend.tv/",
-        "Origin": "https://www.adintrend.tv"
+        "User-Agent": "Mozilla/5.0",
+        "Referer": "https://www.adintrend.tv/"
       }
     });
 
@@ -23,26 +22,16 @@ export default async function handler(req, res) {
       return;
     }
 
-    const m3u8 = match[0];
-
-    const stream = await fetch(m3u8, {
-      headers: {
-        "User-Agent": "Mozilla/5.0",
-        "Referer": "https://www.adintrend.tv/"
-      }
-    });
-
+    const stream = await fetch(match[0]);
     const playlist = await stream.text();
 
-    res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Content-Type","application/vnd.apple.mpegurl");
+    res.setHeader("Access-Control-Allow-Origin","*");
 
     res.send(playlist);
 
-  } catch (err) {
-
-    res.status(500).send(err.toString());
-
+  } catch(e) {
+    res.status(500).send(e.toString());
   }
 
 }
